51592632ab20261707d393f133ee8cc991c22e78
Andrei Zaha <andreiserbanzaha@gmail.com>
2023-07-26 15:11:29 +0300

    leetcode 238_product_of_array_except_self

---
+++ 238_product_of_array_except_self.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+
+//  1   2   3   4
+
+//  1   1   2   6
+
+//  24  12  8   6
+
+class Solution
+{
+public:
+    std::vector<int> productExceptSelf(std::vector<int> &nums)
+    {
+        std::vector<int> res(nums.size(), 1);
+
+        int pre = 1;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            res[i] = pre;
+            pre *= nums[i];
+        }
+
+        int post = 1;
+        for (int i = res.size() - 1; i >= 0; --i)
+        {
+            res[i] *= post;
+            post *= nums[i];
+        }
+
+        return res;
+    }
+};
+
+int main()
+{
+
+    std::vector<int> nums{1, 2, 3, 4}; // [24,12,8,6]
+    // std::vector<int> nums{-1, 1, 0, -3, 3};  // [0,0,9,0,0]
+
+    Solution s;
+    const auto res = s.productExceptSelf(nums);
+
+    return 0;
+}
