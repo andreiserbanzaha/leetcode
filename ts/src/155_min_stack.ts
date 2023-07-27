3ed5e4c5d0a0c48e64115bcc72af2a038a097694
Andrei Zaha <andreiserbanzaha@gmail.com>
2023-07-27 21:59:37 +0300

    leetcode ts folder and setup. 155_min_stack

---
+++ ts/src/155_min_stack.ts
@@ -0,0 +1,48 @@
+class MinStack {
+    constructor() {
+
+    }
+
+    push(val: number): void {
+        this.stack.push(val);
+
+        if (this.stack.length == 1) {
+            this.mins.push(val);
+            return;
+        }
+        
+        const currMin: number = this.mins[this.mins.length - 1];
+        if (currMin > val) {
+            this.mins.push(val);
+        }
+        else {
+            this.mins.push(currMin);
+        }
+
+    }
+
+    pop(): void {
+        this.stack.pop();
+        this.mins.pop();
+    }
+
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin(): number {
+        return this.mins[this.mins.length - 1];
+    }
+
+    stack: number[] = [];
+    mins: number[] = [];
+}
+
+let minStack: MinStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
