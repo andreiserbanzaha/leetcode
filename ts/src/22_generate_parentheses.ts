e3c2c3fb9f466ef70ce6274cabebc37f0e2124ad
Andrei Zaha <andreiserbanzaha@gmail.com>
2023-07-28 19:40:43 +0300

    Leetcode src/22_generate_parentheses

--- ts/src/22_generate_parentheses.ts
+++ ts/src/22_generate_parentheses.ts
@@ -1,14 +1,37 @@
-function generateParenthesis(n: number): string[] {
+function backtrack(
+    n: number,
+    results: string[],
+    curr: string,
+    open: number,
+    closed: number): void {
 
-    while (n > 0) {
+    if (open === n && closed == n) {
+        results.push(curr);
+        return;
     }
 
-    return [];
+    if (open < n) {
+        backtrack(n, results, curr + "(", open + 1, closed);
+    }
+    if (closed < open) {
+        backtrack(n, results, curr + ")", open, closed + 1);
+    }
+
+}
+
+function generateParenthesis(n: number): string[] {
+
+    let results: string[] = [];
+
+    let open: number = 0;
+    let closed: number = 0;
+
+    backtrack(n, results, "", open, closed);
+
+    return results;
 };
 
 {
     const a: string[] = generateParenthesis(3); // ["((()))","(()())","(())()","()(())","()()()"]
     const b: string[] = generateParenthesis(1); // ["()"]
 }
-
-// (
